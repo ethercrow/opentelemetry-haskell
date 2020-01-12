14cff4a669528a11605760df0d0334bfbff7baed
Dmitry Ivanov <ethercrow@gmail.com>
2020-01-12 15:54:41 +0100

    Add pid field to chrome tracing exporter

--- opentelemetry/src/OpenTelemetry/FileExporter.hs
+++ opentelemetry/src/OpenTelemetry/FileExporter.hs
@@ -1,17 +1,34 @@
 module OpenTelemetry.FileExporter where
 
+import Data.Function
+import qualified Data.HashMap.Strict as HM
+import Data.List
+import qualified Data.Text as T
 import OpenTelemetry.Common
 import System.IO
 import Text.Printf
 
+showValue :: TagValue -> String
+showValue (StringTagValue s) = show s
+showValue (IntTagValue i) = show i
+
 showSpan :: Span -> String
 showSpan s@(Span {..}) =
   let (TId tid) = spanTraceId s
+      meta :: String
+      meta =
+        spanTags
+          & HM.toList
+          & map (\(k, v) -> ["\"", T.unpack k, "\":", showValue v])
+          & intersperse [","]
+          & concat
+          & concat
    in printf
-        "{\"ph\":\"B\",\"name\":\"%s\",\"ts\":%d,\"tid\":%d},{\"ph\":\"E\",\"name\":\"%s\",\"ts\":%d,\"tid\":%d},"
+        "{\"ph\":\"B\",\"name\":\"%s\",\"pid\":1,\"ts\":%d,\"tid\":%d,\"meta\":{%s}},{\"ph\":\"E\",\"name\":\"%s\",\"pid\":1,\"ts\":%d,\"tid\":%d},"
         spanOperation
         (div spanStartedAt 1000)
         tid
+        meta
         spanOperation
         (div spanFinishedAt 1000)
         tid
