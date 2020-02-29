49130bc4c729e204a7568a40ecdf1774dec506a6
Dmitry Ivanov <ethercrow@gmail.com>
2020-02-29 21:07:07 +0100

    Use progname as a default for the service name

--- opentelemetry-lightstep/src/OpenTelemetry/LightStep/Config.hs
+++ opentelemetry-lightstep/src/OpenTelemetry/LightStep/Config.hs
@@ -33,13 +33,14 @@ getEnvTagsWithPrefix prefix =
 
 getEnvConfig :: MonadIO m => m (Maybe LightStepConfig)
 getEnvConfig = liftIO $ do
+  prog_name <- getProgName
   maybe_token_from_env <- lookupOneOfEnvs ["LIGHTSTEP_TOKEN", "LIGHTSTEP_ACCESS_TOKEN", "OPENTRACING_LIGHTSTEP_ACCESS_TOKEN"]
   global_tags <- getEnvTagsWithPrefix "OPENTRACING_TAG_"
   case maybe_token_from_env of
     Just t -> do
       host <- fromMaybe "ingest.lightstep.com" <$> lookupOneOfEnvs ["LIGHTSTEP_HOST", "OPENTRACING_LIGHTSTEP_COLLECTOR_HOST"]
       port <- maybe 443 read <$> lookupOneOfEnvs ["LIGHTSTEP_PORT", "OPENTRACING_LIGHTSTEP_COLLECTOR_PORT"]
-      service <- fromMaybe "example-haskell-service" <$> lookupOneOfEnvs ["LIGHTSTEP_SERVICE", "OPENTRACING_LIGHTSTEP_COMPONENT_NAME"]
+      service <- fromMaybe prog_name <$> lookupOneOfEnvs ["LIGHTSTEP_SERVICE", "OPENTRACING_LIGHTSTEP_COMPONENT_NAME"]
       pure $ Just $ LightStepConfig host port (T.pack t) (T.pack service) global_tags 5 4096
     Nothing -> do
       hPutStrLn stderr "LIGHTSTEP_ACCESS_TOKEN environment variable not defined"
