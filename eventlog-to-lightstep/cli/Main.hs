22070bba042652e9de52d918ce010007274c820e
Dmitry Ivanov <ethercrow@gmail.com>
2020-04-08 01:44:28 +0200

    Connect spans across threads

--- eventlog-to-lightstep/cli/Main.hs
+++ eventlog-to-lightstep/cli/Main.hs
@@ -46,10 +46,11 @@ work exporter input = do
   go (initialState smgen) decodeEventLog
   where
     go s (Produce event next) = do
-      -- print (evCap event, evSpec event)
+      print (evCap event, evSpec event)
       let (s', sps) = processEvent event s
       export exporter sps
-      -- mapM_ (putStrLn . ("emit " <>) . show) sps
+      print s'
+      mapM_ (putStrLn . ("emit " <>) . show) sps
       go s' next
     go s d@(Consume consume) = do
       eof <- hIsEOF input
@@ -69,31 +70,38 @@ data State
       { originTimestamp :: Timestamp,
         threadMap :: IM.IntMap ThreadId,
         spanStacks :: HM.HashMap ThreadId (NonEmpty Span),
+        traceMap :: HM.HashMap ThreadId TraceId,
         randomGen :: R.SMGen
       }
   deriving (Show)
 
 initialState :: R.SMGen -> State
-initialState = S 0 mempty mempty
+initialState = S 0 mempty mempty mempty
 
 processEvent :: Event -> State -> (State, [Span])
-processEvent (Event ts ev mcap) st@(S o tm ss r) =
-  let now = o + ts
-      mtid = mcap >>= flip IM.lookup tm
-   in case (ev, mcap, mtid) of
+processEvent (Event ts ev m_cap) st@(S {..}) =
+  let now = originTimestamp + ts
+      m_thread_id = m_cap >>= flip IM.lookup threadMap
+      m_trace_id = m_thread_id >>= flip HM.lookup traceMap
+   in case (ev, m_cap, m_thread_id) of
+        (CreateThread new_tid, _, _) ->
+          case m_trace_id of
+            Just trace_id -> (st {traceMap = HM.insert new_tid trace_id traceMap}, [])
+            _ -> (st, [])
         (RunThread tid, Just cap, _) ->
-          -- TODO(divanov): maintain the thread ancestry
-          (S o (IM.insert cap tid tm) ss r, [])
+          (st { threadMap = IM.insert cap tid threadMap}, [])
         (StopThread tid tstatus, Just cap, _)
-          | isTerminalThreadStatus tstatus -> (S o (IM.delete cap tm) ss r, [])
+          | isTerminalThreadStatus tstatus -> (st { threadMap = IM.delete cap threadMap}, [])
         (StartGC, _, _) -> (st, []) -- TODO(divanov): push a gc span on every stack
         (GCStatsGHC {gen}, _, _) -> (modifyAllSpans (setTag "gen" gen) st, [])
         (EndGC, _, _) -> (st, []) -- TODO(divanov): pop the gc span from every stack
-        (UserMessage {msg}, Just cap, _) -> case T.words msg of
-          ["ot1", "begin", "span", name] -> (pushSpan st cap name now, [])
-          ["ot1", "end", "span"] -> popSpan st cap now
-          ["ot1", "set", "tag", k, v] -> (modifySpan st cap (setTag k v), [])
-          ["ot1", "add", "event", k, v] -> (modifySpan st cap (addEvent now k v), [])
+        (HeapAllocated {allocBytes}, _, Just tid) ->
+          (modifySpan st tid (addEvent now "heap_alloc_bytes" (showT allocBytes)), [])
+        (UserMessage {msg}, _, Just tid) -> case T.words msg of
+          ["ot1", "begin", "span", name] -> (pushSpan st tid name now, [])
+          ["ot1", "end", "span"] -> popSpan st tid now
+          ["ot1", "set", "tag", k, v] -> (modifySpan st tid (setTag k v), [])
+          ["ot1", "add", "event", k, v] -> (modifySpan st tid (addEvent now k v), [])
           _ -> (st, [])
         _ -> (st, [])
 
@@ -118,26 +126,28 @@ modifyAllSpans f st =
           (spanStacks st)
     }
 
-modifySpan :: HasCallStack => State -> Int -> (Span -> Span) -> State
-modifySpan st cap f =
+modifySpan :: HasCallStack => State -> ThreadId -> (Span -> Span) -> State
+modifySpan st tid f =
   st
     { spanStacks =
-        let tid = threadMap st IM.! cap
-         in HM.update (\(sp :| sps) -> Just (f sp :| sps)) tid (spanStacks st)
+         HM.update (\(sp :| sps) -> Just (f sp :| sps)) tid (spanStacks st)
     }
 
-pushSpan :: HasCallStack => State -> Int -> T.Text -> OTel.Timestamp -> State
-pushSpan st cap name timestamp = st {spanStacks = new_stacks, randomGen = r'}
+pushSpan :: HasCallStack => State -> ThreadId -> T.Text -> OTel.Timestamp -> State
+pushSpan st tid name timestamp = st {spanStacks = new_stacks, randomGen = new_randomGen, traceMap = new_traceMap}
   where
     maybe_parent = NE.head <$> HM.lookup tid (spanStacks st)
     new_stacks = HM.alter f tid (spanStacks st)
     f Nothing = Just $ sp :| []
     f (Just sps) = Just $ cons sp sps
-    tid = threadMap st IM.! cap
-    (sid, r') = R.nextWord64 (randomGen st)
+    (sid, new_randomGen) = R.nextWord64 (randomGen st)
+    (new_traceMap, trace_id) = case (maybe_parent, HM.lookup tid (traceMap st)) of
+      (Just parent, _) -> (traceMap st, spanTraceId parent)
+      (_, Just trace_id) -> (traceMap st, trace_id)
+      _ -> let new_trace_id = TId sid in (HM.insert tid new_trace_id (traceMap st), new_trace_id)
     sp =
       Span
-        { spanContext = SpanContext (SId sid) (maybe (TId sid) spanTraceId maybe_parent),
+        { spanContext = SpanContext (SId sid) trace_id,
           spanOperation = name,
           spanStartedAt = timestamp,
           spanFinishedAt = 0,
@@ -147,17 +157,19 @@ pushSpan st cap name timestamp = st {spanStacks = new_stacks, randomGen = r'}
           spanParentId = spanId <$> maybe_parent
         }
 
-popSpan :: HasCallStack => State -> Int -> OTel.Timestamp -> (State, [Span])
-popSpan st cap now = (st {spanStacks = new_stacks}, [sp {spanFinishedAt = now}])
+popSpan :: HasCallStack => State -> ThreadId -> OTel.Timestamp -> (State, [Span])
+popSpan st tid now = (st {spanStacks = new_stacks, traceMap = new_traceMap}, [sp {spanFinishedAt = now}])
   where
     sp :| new_stack = spanStacks st HM.! tid
-    new_stacks = case new_stack of
-      [] -> HM.delete tid (spanStacks st)
-      (x : xs) -> HM.insert tid (x :| xs) (spanStacks st)
-    tid = threadMap st IM.! cap
+    (new_traceMap, new_stacks) = case new_stack of
+      [] -> (HM.delete tid (traceMap st), HM.delete tid (spanStacks st))
+      (x : xs) -> (traceMap st, HM.insert tid (x :| xs) (spanStacks st))
 
 isTerminalThreadStatus :: ThreadStopStatus -> Bool
 isTerminalThreadStatus HeapOverflow = True
 isTerminalThreadStatus StackOverflow = True
 isTerminalThreadStatus ThreadFinished = True
 isTerminalThreadStatus _ = False
+
+showT :: Show a => a -> T.Text
+showT = T.pack . show
