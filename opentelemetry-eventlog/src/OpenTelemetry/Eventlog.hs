2a090c7be55193ffb7f95baa3e3d9cd3b3aecb0b
Dmitry Ivanov <ethercrow@gmail.com>
2020-04-08 00:31:21 +0200

    Unify the eventlog opentelemetry format between the libraries

--- opentelemetry-eventlog/src/OpenTelemetry/Eventlog.hs
+++ opentelemetry-eventlog/src/OpenTelemetry/Eventlog.hs
@@ -13,16 +13,16 @@ import Debug.Trace
 import Text.Printf
 
 beginSpan :: String -> IO ()
-beginSpan operation = traceEventIO (printf "beginSpan %s" operation)
+beginSpan operation = traceEventIO (printf "ot1 begin span %s" operation)
 
 endSpan :: IO ()
-endSpan = traceEventIO (printf "endSpans")
+endSpan = traceEventIO (printf "ot1 end span")
 
 setTag :: String -> String -> IO ()
-setTag k v = traceEventIO (printf "setTag %s %s" k v)
+setTag k v = traceEventIO (printf "ot1 set tag %s %s" k v)
 
 addEvent :: String -> String -> IO ()
-addEvent k v = traceEventIO (printf "addEvent %s %s" k v)
+addEvent k v = traceEventIO (printf "ot1 add event %s %s" k v)
 
 withSpan :: forall m a. (MonadIO m, MonadMask m) => String -> m a -> m a
 withSpan operation action =
