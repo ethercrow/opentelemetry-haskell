ceeb05e1d8c9755d83461fc3a7b5f2a1724b3245
Tristan Cacqueray <tdecacqu@redhat.com>
2020-06-05 12:18:39 +0000

    eventlog-to-tracy: add Tracy install instructions

--- opentelemetry-extra/exe/eventlog-to-tracy/Main.hs
+++ opentelemetry-extra/exe/eventlog-to-tracy/Main.hs
@@ -1,19 +1,28 @@
 module Main where
 
+import Data.List (intercalate)
 import OpenTelemetry.ChromeExporter
 import OpenTelemetry.EventlogStreaming_Internal
 import OpenTelemetry.Exporter
 import System.Clock
+import System.Directory (findExecutable)
 import System.Environment (getArgs)
 import System.Exit
 import System.Process
-import System.Directory
 
 help :: IO ()
 help = do
   putStrLn "Converts eventlog to Tracy format and launches the viewer."
   putStrLn "Path to eventlog is expected."
   exitFailure
+
+tracyInstallInstructions :: String
+tracyInstallInstructions =
+  intercalate
+    ", "
+    [ "https://github.com/wolfpld/tracy from source",
+      "or get it from AUR or nixpkgs"
+    ]
 
 callProcessOrExplain :: FilePath -> String -> [String] -> IO ()
 callProcessOrExplain filename instruction args = do
@@ -36,12 +45,12 @@ main :: IO ()
 main = do
   args <- getArgs
   case args of
-    ["-h"]  -> help
+    ["-h"] -> help
     ["--help"] -> help
     [eventlogFile] -> do
       let chromeFile = eventlogFile ++ ".trace.json"
       let tracyFile = eventlogFile ++ ".tracy"
       eventlogToChrome eventlogFile chromeFile
-      callProcessOrExplain "import-chrome" "chrome" [chromeFile, tracyFile]
-      callProcessOrExplain "Tracy" "Tracy" [tracyFile]
+      callProcessOrExplain "import-chrome" tracyInstallInstructions [chromeFile, tracyFile]
+      callProcessOrExplain "Tracy" tracyInstallInstructions [tracyFile]
     _ -> help
