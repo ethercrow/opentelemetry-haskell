13e12af8e2b27897786c96d8cd7b5b813492bd65
Tristan Cacqueray <tdecacqu@redhat.com>
2020-06-05 12:27:35 +0000

    ChromeExporter: add eventlogToChrome function

--- opentelemetry-extra/exe/eventlog-to-tracy/Main.hs
+++ opentelemetry-extra/exe/eventlog-to-tracy/Main.hs
@@ -1,10 +1,7 @@
 module Main where
 
 import Data.List (intercalate)
-import OpenTelemetry.ChromeExporter
-import OpenTelemetry.EventlogStreaming_Internal
-import OpenTelemetry.Exporter
-import System.Clock
+import OpenTelemetry.ChromeExporter (eventlogToChrome)
 import System.Directory (findExecutable)
 import System.Environment (getArgs)
 import System.Exit
@@ -33,14 +30,6 @@ callProcessOrExplain filename instruction args = do
       putStrLn $ filename <> ": command not found, please install " <> instruction
       exitFailure
 
-eventlogToChrome :: FilePath -> FilePath -> IO ()
-eventlogToChrome eventlogFile chromeFile = do
-  putStrLn $ "Converting " <> eventlogFile <> " to " <> chromeFile <> "..."
-  exporter <- createChromeSpanExporter chromeFile
-  origin_timestamp <- fromIntegral . toNanoSecs <$> getTime Realtime
-  work origin_timestamp exporter $ EventLogFilename eventlogFile
-  shutdown exporter
-
 main :: IO ()
 main = do
   args <- getArgs
@@ -50,6 +39,7 @@ main = do
     [eventlogFile] -> do
       let chromeFile = eventlogFile ++ ".trace.json"
       let tracyFile = eventlogFile ++ ".tracy"
+      putStrLn $ "Converting " <> eventlogFile <> " to " <> chromeFile <> "..."
       eventlogToChrome eventlogFile chromeFile
       callProcessOrExplain "import-chrome" tracyInstallInstructions [chromeFile, tracyFile]
       callProcessOrExplain "Tracy" tracyInstallInstructions [tracyFile]
