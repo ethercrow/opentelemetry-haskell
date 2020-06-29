f103d8748f7cae6196dbf312d5a0bc8311bf592f
Michalis Pardalos <m.pardalos@gmail.com>
2020-06-29 13:42:43 +0100

    Add Show, Eq instances for Instrument, SomeInstrument

--- opentelemetry/src/OpenTelemetry/Instruments.hs
+++ opentelemetry/src/OpenTelemetry/Instruments.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE StandaloneDeriving #-}
 module OpenTelemetry.Instruments where
 
 data Synchronicity = Synchronous | Asynchronous
@@ -23,6 +24,25 @@ data Instrument (s :: Synchronicity) (a :: Additivity) (m :: Monotonicity) t whe
   SumObserver       :: String -> SumObserver t
   UpDownSumObserver :: String -> UpDownSumObserver t
   ValueObserver     :: String -> ValueObserver t
+
+data SomeInstrument t = forall s a m. SomeInstrument (Instrument s a m t)
+
+deriving instance Show (Instrument s a m t)
+deriving instance Eq (Instrument s a m t)
+
+instance Show (SomeInstrument t) where
+  show (SomeInstrument i) = show i
+
+instance Eq (SomeInstrument t) where
+  (SomeInstrument i1) == (SomeInstrument i2) = case (i1, i2) of
+    (Counter s1, Counter s2) -> s1 == s2
+    (UpDownCounter s1, UpDownCounter s2) -> s1 == s2
+    (ValueRecorder s1, ValueRecorder s2) -> s1 == s2
+    (SumObserver s1, SumObserver s2) -> s1 == s2
+    (UpDownSumObserver s1, UpDownSumObserver s2) -> s1 == s2
+    (ValueObserver s1, ValueObserver s2) -> s1 == s2
+    (_, _) -> False
+
 
 instrumentName :: Instrument s a m t -> String
 instrumentName (Counter n) = n
