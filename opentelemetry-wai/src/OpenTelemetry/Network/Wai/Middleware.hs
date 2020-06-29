aade83e4cfc52449cb909954bf3fd848fcca4062
Michalis Pardalos <m.pardalos@gmail.com>
2020-06-29 13:00:41 +0100

    Add request counter to wai middleware

--- opentelemetry-wai/src/OpenTelemetry/Network/Wai/Middleware.hs
+++ opentelemetry-wai/src/OpenTelemetry/Network/Wai/Middleware.hs
@@ -7,13 +7,18 @@ import Network.HTTP.Types
 import Network.Wai
 import OpenTelemetry.Eventlog
 import OpenTelemetry.Propagation
+import OpenTelemetry.Instruments
 
 -- Semantic conventions for HTTP spans:
 -- https://github.com/open-telemetry/opentelemetry-specification/blob/master/specification/trace/semantic_conventions/http.md
 
+requestCounter :: Counter Int
+requestCounter = Counter "requests"
+
 middleware :: Application -> Application
 middleware app = \req sendResp -> do
   withSpan "WAI handler" $ \sp -> do
+    add requestCounter 1
     case propagateFromHeaders w3cTraceContext (requestHeaders req) of
       Just ctx -> setParentSpanContext sp ctx
       _ -> pure ()
