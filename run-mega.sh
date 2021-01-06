df99bec5c5d4ce63f36b8331186a9dff0bd60095
Dmitry Ivanov <ethercrow@gmail.com>
2021-01-06 22:20:07 +0100

    Fixing CI

--- run-mega.sh
+++ run-mega.sh
@@ -1,27 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-BUILD_OPTIONS=""
-case "$1" in
-    -h|--help)
-        cat<<EOF
-Builds, runs mega example and shows charts.
-
-  -b --binary
-  use lazy ByteString instead String
-
-EOF
-        exit 1;;
-    -b|--binary)
-        BUILD_OPTIONS="--flag megaexample:binary"
-        ;;
-    "") ;;
-    *)
-        echo "Bad option [$@] see --help"
-        exit 1;;
-esac
 
 FILE=${FILE:-megaexample.eventlog}
 rm -f megaexample.eventlog megaexample.eventlog.trace.json megaexample.eventlog.tracy
-stack install $BUILD_OPTIONS \
+env STACK_YAML="stack-8.10.yaml" stack install \
     && opentelemetry-megaexample +RTS -T -l -ol${FILE} \
     && eventlog-to-tracy ${FILE}
